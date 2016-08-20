5e88133a6d7ef272ca810b1833c0394462c0ed47
jflinter <jflinter11@gmail.com>
2016-08-20 02:04:46 -0400

    build step

--- fetch_products.rb
+++ fetch_products.rb
@@ -8,43 +8,7 @@ Dotenv.load
 
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
-products = Stripe::Product.all
+products = Stripe::Product.all(limit: 100).sort_by(&:name)
 
-FileUtils.rm_rf(Dir.glob("Stone/Crystals.xcassets/*.imageset"))
-
-products.each do |product|
-  url = product.images.first
-  filename = "#{product.id}.jpg"
-  dirname = "Stone/Crystals.xcassets/#{product.id}.imageset"
-  FileUtils.mkdir_p(dirname)
-  File.open("#{dirname}/#{filename}", 'w') do |file|
-    file.write(open(url).read)
-  end
-  json_string = JSON.pretty_generate({
-    images: [
-      {
-        idiom: "iphone",
-        scale: "1x"
-      },
-      {
-        idiom: "iphone",
-        filename: filename,
-        scale: "2x"
-      },
-      {
-        idiom: "iphone",
-        scale: "3x"
-      }
-    ],
-    info: {
-      version: 1,
-      author: "xcode"
-    }
-  }).gsub(/":/, '" :')
-  File.open("#{dirname}/contents.json", 'w') do |file|
-    file.write(json_string)
-  end
-end
-
-json = JSON.pretty_generate(products.values[1])
-File.open('Stone/products.json', 'w') { |file| file.write(json) }
+json = JSON.pretty_generate(products)
+File.open('products.json', 'w') { |file| file.write(json) }
